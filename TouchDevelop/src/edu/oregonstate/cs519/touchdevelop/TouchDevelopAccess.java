ae288a361980779ec37d490531499c04e61b768f
Caius Brindescu <brind@illinois.edu>
2013-10-31 13:33:17 -0700

    Added a way to get some scripts

--- TouchDevelop/src/edu/oregonstate/cs519/touchdevelop/TouchDevelopAccess.java
+++ TouchDevelop/src/edu/oregonstate/cs519/touchdevelop/TouchDevelopAccess.java
@@ -1,5 +1,13 @@
 package edu.oregonstate.cs519.touchdevelop;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.json.simple.JSONValue;
+
+import com.github.kevinsawicki.http.HttpRequest;
+
 /**
  * Class for easily accessing the REST API of the TouchDevelop platform.
  * 
@@ -10,6 +18,32 @@ package edu.oregonstate.cs519.touchdevelop;
  */
 class TouchDevelopAccess {
 
+	private static final String API_ROOT = "http://www.touchdevelop.com/api/";
+
 	private TouchDevelopAccess() {
 	}
+
+	/**
+	 * Gets the first list of scripts returned by the TouchDevelop API.
+	 * 
+	 * @return a list of scripts
+	 */
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public static List<Script> getScripts() {
+		String call = API_ROOT + "scripts";
+		Map<String, Object> map = doCall(call);
+		List<Map> listOfMapScripts = (List<Map>) map.get("items");
+		List<Script> scripts = new ArrayList<Script>();
+		for (Map<String, Object> mapScript : listOfMapScripts) {
+			scripts.add(new Script(mapScript));
+		}
+		return scripts;
+	}
+
+	private static Map<String, Object> doCall(String call) {
+		String response = HttpRequest.get(call).body();
+		Map<String, Object> map = (Map<String, Object>) JSONValue.parse(response);
+		return map;
+	}
+	
 }
--- TouchDevelop/test/edu/oregonstate/cs519/touchdevelop/TouchDevelopAccessTest.java
+++ TouchDevelop/test/edu/oregonstate/cs519/touchdevelop/TouchDevelopAccessTest.java
@@ -1,5 +1,16 @@
 package edu.oregonstate.cs519.touchdevelop;
 
-public class TouchDevelopAccessTest {
+import static org.junit.Assert.*;
 
+import java.util.List;
+
+import org.junit.Test;
+
+public class TouchDevelopAccessTest {
+	
+	@Test
+	public void testGetScripts() {
+		List<Script> scripts = (List<Script>) TouchDevelopAccess.getScripts();
+		assertTrue(scripts.size() > 0);
+	}
 }
