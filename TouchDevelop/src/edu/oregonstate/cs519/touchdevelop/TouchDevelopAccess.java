424c5145a19acfa380f7f8e3c8bc0ae5cd0d092b
Caius Brindescu <brind@illinois.edu>
2013-10-31 13:02:56 -0700

    Added stub for the TouchDevelopAccess class.

---
+++ TouchDevelop/src/edu/oregonstate/cs519/touchdevelop/TouchDevelopAccess.java
@@ -0,0 +1,15 @@
+package edu.oregonstate.cs519.touchdevelop;
+
+/**
+ * Class for easily accessing the REST API of the TouchDevelop platform.
+ * 
+ * It is intended to be used a static function thing.
+ * 
+ * @author caius
+ * 
+ */
+class TouchDevelopAccess {
+
+	private TouchDevelopAccess() {
+	}
+}
