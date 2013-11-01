4df3b8e44d3a5eb987101daba9fe050b7d70f5dc
Caius Brindescu <brind@illinois.edu>
2013-10-31 21:38:45 -0700

    Added a quick main class

---
+++ TouchDevelop/src/edu/oregonstate/cs519/touchdevelop/main/Main.java
@@ -0,0 +1,29 @@
+package edu.oregonstate.cs519.touchdevelop.main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.oregonstate.cs519.touchdevelop.Script;
+import edu.oregonstate.cs519.touchdevelop.ScriptLibrary;
+import edu.oregonstate.cs519.touchdevelop.TouchDevelopAccess;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ScriptLibrary instance = ScriptLibrary.getInstance();
+		instance.setPersitanceDestination("~/Downloads/TouchDevelopScripts");
+		
+		List<Script> scripts = TouchDevelopAccess.getScripts(2000);
+		List<Script> scriptsWithTwoOrMoreSuccessors = new ArrayList<Script>();
+		for (Script script : scripts) {
+			instance.addScript(script);
+			List<Script> successors = script.getSuccessors();
+			if (successors.size() > 2);
+				scriptsWithTwoOrMoreSuccessors.add(script);
+		}
+		
+		System.out.println("Number of scripts with two or more successors is: " + scriptsWithTwoOrMoreSuccessors.size());
+		System.out.println("This is " + scriptsWithTwoOrMoreSuccessors.size()/2000 * 100 + "% of all scripts");
+	}
+
+}
