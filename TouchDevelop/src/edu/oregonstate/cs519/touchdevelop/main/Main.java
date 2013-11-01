219f001eb8735ff8bab5ffe6a1cee6fe332c3bd1
Caius Brindescu <brind@illinois.edu>
2013-10-31 22:53:31 -0700

    Extracted the number of scripts

--- TouchDevelop/src/edu/oregonstate/cs519/touchdevelop/main/Main.java
+++ TouchDevelop/src/edu/oregonstate/cs519/touchdevelop/main/Main.java
@@ -10,10 +10,12 @@ import edu.oregonstate.cs519.touchdevelop.TouchDevelopAccess;
 public class Main {
 
 	public static void main(String[] args) {
+		int noOfScripts = 5000;
+		
 		ScriptLibrary instance = ScriptLibrary.getInstance();
 		instance.setPersitanceDestination("/Users/caius/Downloads/TouchDevelopScripts");
 		
-		List<Script> scripts = TouchDevelopAccess.getScripts(20);
+		List<Script> scripts = TouchDevelopAccess.getScripts(noOfScripts);
 		List<Script> scriptsWithTwoOrMoreSuccessors = new ArrayList<Script>();
 		for (Script script : scripts) {
 			instance.addScript(script);
@@ -25,7 +27,7 @@ public class Main {
 		instance.save();
 		
 		System.out.println("Number of scripts with two or more successors is: " + scriptsWithTwoOrMoreSuccessors.size());
-		System.out.println("This is " + scriptsWithTwoOrMoreSuccessors.size()/2000 * 100 + "% of all scripts");
+		System.out.println("This is " + scriptsWithTwoOrMoreSuccessors.size()/noOfScripts * 100 + "% of all scripts");
 	}
 
 }
