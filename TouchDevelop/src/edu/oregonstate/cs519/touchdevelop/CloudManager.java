92e593302de1767c7c8f38960e58559f14f90b4e
Caius Brindescu <brind@illinois.edu>
2013-11-03 14:28:46 -0800

    Added a CloudManager

---
+++ TouchDevelop/src/edu/oregonstate/cs519/touchdevelop/CloudManager.java
@@ -0,0 +1,27 @@
+package edu.oregonstate.cs519.touchdevelop;
+
+import java.util.List;
+
+public class CloudManager implements ScriptManager {
+
+	@Override
+	public void addScript(Script script) {
+	}
+
+	@Override
+	public Script getScript(String id) {
+		return TouchDevelopAccess.getScript(id);
+	}
+
+	@Override
+	public List<Script> getScripts(int number) {
+		return TouchDevelopAccess.getScripts(number);
+	}
+
+	@Override
+	public List<Script> getAllScripts() {
+		return null;
+		//return TouchDevelopAccess.getAllScripts();
+	}
+
+}
