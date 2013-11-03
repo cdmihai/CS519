021d0b589448374db7bd8227729ddbd93ef3a942
Caius Brindescu <brind@illinois.edu>
2013-11-03 14:16:32 -0800

    Implementing the ScriptManager interface in the ScriptLibrary

--- TouchDevelop/src/edu/oregonstate/cs519/touchdevelop/ScriptLibrary.java
+++ TouchDevelop/src/edu/oregonstate/cs519/touchdevelop/ScriptLibrary.java
@@ -1,5 +1,34 @@
 package edu.oregonstate.cs519.touchdevelop;
 
-public class ScriptLibrary {
+import java.util.List;
 
+public class ScriptLibrary implements ScriptManager {
+	
+	public ScriptLibrary() {
+	}
+
+	@Override
+	public void addScript(Script script) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Script getScript(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Script> getScripts(int number) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Script> getAllScripts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 }
