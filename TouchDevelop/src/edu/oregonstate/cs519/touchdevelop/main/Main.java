c7a5c7b455badbc3818faf4cc5695a9707b1334d
Caius Brindescu <brind@illinois.edu>
2013-11-06 11:33:37 -0800

    Redesigned main

--- TouchDevelop/src/edu/oregonstate/cs519/touchdevelop/main/Main.java
+++ TouchDevelop/src/edu/oregonstate/cs519/touchdevelop/main/Main.java
@@ -1,5 +1,11 @@
 package edu.oregonstate.cs519.touchdevelop.main;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 import java.util.List;
 
 import edu.oregonstate.cs519.touchdevelop.CloudManager;
@@ -12,17 +18,34 @@ public class Main {
 	public static void main(String[] args) {
 		int noOfScriptsWithMoreThanOneSuccessor = 0;
 
-		MemoryLibrary library = new MemoryLibrary(new FileLibrary("/Users/caius/Downloads/TouchDevelopScripts",new CloudManager()));
+		FileLibrary fileLibrary = new FileLibrary("/Users/caius/Downloads/TouchDevelopScripts",new CloudManager());
+		MemoryLibrary library = MemoryLibrary.getInstance();
+		library.setNext(fileLibrary);
 		
-		List<Script> allScripts = library.getAllScripts();
-		for (Script script : allScripts) {
+		List<Script> knownScripts = fileLibrary.getKnownScripts();
+		int all = knownScripts.size();
+		List<Script> scriptsWithMoreThanTwoSuccessors = new ArrayList<Script>();
+		int current = 0;
+		for (Script script : knownScripts) {
+			current++;
+			System.out.println("Processing " + current + "/" + all);
 			List<Script> successors = script.getSuccessors();
 			if (successors.size() > 1)
-				noOfScriptsWithMoreThanOneSuccessor++;
-			library.addScript(script);
+				scriptsWithMoreThanTwoSuccessors.add(script);
 		}
 		
-		System.out.println(noOfScriptsWithMoreThanOneSuccessor);
+		System.out.println((float)(scriptsWithMoreThanTwoSuccessors.size())/(float)(knownScripts.size()));
+		
+		for (Script script : scriptsWithMoreThanTwoSuccessors) {
+			try {
+				Path filePath = Paths.get("good.txt");
+				if (!Files.exists(filePath))
+					Files.createFile(filePath);
+				Files.write(filePath, script.getID().getBytes(), StandardOpenOption.APPEND);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
