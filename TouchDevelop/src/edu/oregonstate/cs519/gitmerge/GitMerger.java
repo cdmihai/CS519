48131af81e65c892cfef772c7774856e74c642f7
Caius Brindescu <brind@illinois.edu>
2013-11-08 18:40:29 -0800

    Added stubs for the Git merger

---
+++ TouchDevelop/src/edu/oregonstate/cs519/gitmerge/GitMerger.java
@@ -0,0 +1,11 @@
+package edu.oregonstate.cs519.gitmerge;
+
+/**
+ * This class merges two branches using the Git default algorithm.
+ * 
+ * @author caius
+ *
+ */
+public class GitMerger {
+
+}
---
+++ TouchDevelop/test/edu/oregonstate/cs519/gitmerge/GitMergerTest.java
@@ -0,0 +1,5 @@
+package edu.oregonstate.cs519.gitmerge;
+
+public class GitMergerTest {
+
+}
