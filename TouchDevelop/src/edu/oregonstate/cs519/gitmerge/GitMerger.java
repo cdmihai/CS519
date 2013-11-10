5efcd9099aff017867910e0ac192bd580ba7c8fc
Caius Brindescu <brind@illinois.edu>
2013-11-09 19:08:18 -0800

    Added stub for merge method

--- TouchDevelop/src/edu/oregonstate/cs519/gitmerge/GitMerger.java
+++ TouchDevelop/src/edu/oregonstate/cs519/gitmerge/GitMerger.java
@@ -1,5 +1,7 @@
 package edu.oregonstate.cs519.gitmerge;
 
+import org.eclipse.jgit.api.MergeResult;
+
 /**
  * This class merges two branches using the Git default algorithm.
  * 
@@ -8,4 +10,7 @@ package edu.oregonstate.cs519.gitmerge;
  */
 public class GitMerger {
 
+	public static MergeResult merge(String base, String a, String b){
+		return null;
+	}
 }
--- TouchDevelop/test/edu/oregonstate/cs519/gitmerge/GitMergerTest.java
+++ TouchDevelop/test/edu/oregonstate/cs519/gitmerge/GitMergerTest.java
@@ -1,5 +1,12 @@
 package edu.oregonstate.cs519.gitmerge;
 
+import org.junit.Test;
+
 public class GitMergerTest {
+	
+	@Test
+	public void testMergeNoConflict() {
+		
+	}
 
 }
