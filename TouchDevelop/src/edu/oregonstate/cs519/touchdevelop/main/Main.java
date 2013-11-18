6230e71bc97494969b53099530f73dc15de41df0
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2013-11-17 20:17:05 -0800

    Completely new main

--- TouchDevelop/src/edu/oregonstate/cs519/touchdevelop/main/Main.java
+++ TouchDevelop/src/edu/oregonstate/cs519/touchdevelop/main/Main.java
@@ -3,10 +3,14 @@ package edu.oregonstate.cs519.touchdevelop.main;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.eclipse.jgit.api.MergeResult;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 import edu.oregonstate.cs519.gitmerge.GitMerger;
 import edu.oregonstate.cs519.touchdevelop.CloudManager;
@@ -17,32 +21,28 @@ import edu.oregonstate.cs519.touchdevelop.Script;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-
-		FileLibrary fileLibrary = new FileLibrary("/Volumes/RAM Disk/TouchDevelopScripts",new CloudManager());
-		MemoryLibrary library = MemoryLibrary.getInstance();
-		library.setNext(fileLibrary);
 		
-		List<String> lines = Files.readAllLines(Paths.get("good.txt"), Charset.defaultCharset());
-		for (String scriptID : lines) {
-			Script script = library.getScript(scriptID);
-			List<Script> successors = script.getSuccessors();
-			String base = script.getText();
-			List<Script> seen = new ArrayList<>();
-			for (Script successor : successors) {
-				String a = successor.getText();
-				seen.add(successor);
-				for (Script successor2 : successors) {
-					if (successor2.equals(successor))
-						continue;
-					if (seen.contains(successor2))
-						continue;
-					String b = successor2.getText();
-					MergeResult result = GitMerger.merge(base, a, b);
-					System.out.println(successor.getID() + " with " + successor2.getID() + " => " + result.getMergeStatus().toString());
-				}
+		MemoryLibrary library = new MemoryLibrary("../results/TDScripts.json");
+		
+		List<String> lines = Files.readAllLines(Paths.get("../results/conflict-100-mr.txt"), Charset.defaultCharset());
+		for (String line : lines) {
+			String[] split = line.split(",");
+			String aScriptID = split[0];
+			String bScriptID = split[1];
+			
+			Script aScript = library.getScript(aScriptID);
+			Script bScript = library.getScript(bScriptID);
+
+			String baseScriptID = aScript.getRootID();
+			Script baseScript = library.getScript(baseScriptID);
+			
+			try {
+				MergeResult mergeResult = GitMerger.merge(baseScript.getText(), aScript.getText(), bScript.getText(), false, aScriptID + "-" + bScriptID);
+				System.out.println(aScriptID + "+" + bScriptID + " => " + mergeResult.getMergeStatus());
+			} catch (NullPointerException e) {
+				System.out.println("Aborted: " + aScriptID + "+" + baseScriptID);
 			}
 		}
-		
 	}
 
 }
