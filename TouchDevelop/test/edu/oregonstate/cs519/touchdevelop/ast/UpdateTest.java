af2f2a956f6bc444f9f18cf61ac438a7a24ba79f
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2013-11-20 11:11:25 -0800

    Formatting

--- TouchDevelop/test/edu/oregonstate/cs519/touchdevelop/ast/UpdateTest.java
+++ TouchDevelop/test/edu/oregonstate/cs519/touchdevelop/ast/UpdateTest.java
@@ -77,27 +77,27 @@ public class UpdateTest {
 		String afterJSON = program.getJSON();
 		return afterJSON;
 	}
-	
+
 	@Test
 	public void testAddGlobalVariable() {
-		String updateJSON = "{\n" + 
-				"        \"name\": \"s\",\n" + 
-				"        \"comment\": \"\",\n" + 
-				"        \"type\": \"String\",\n" + 
-				"        \"isReadonly\": false,\n" + 
-				"        \"isTransient\": true,\n" + 
-				"        \"nodeType\": \"data\"\n" + 
-				"      }";
+		String updateJSON = "{\n" + "        \"name\": \"s\",\n"
+				+ "        \"comment\": \"\",\n"
+				+ "        \"type\": \"String\",\n"
+				+ "        \"isReadonly\": false,\n"
+				+ "        \"isTransient\": true,\n"
+				+ "        \"nodeType\": \"data\"\n" + "      }";
 		String nodeID = "wAHj4rpF7s1v6qxVEDsWsDWL";
 		String after = applyUpdateToProgram(updateJSON, nodeID);
-		assertEquals("{\"isLibrary\":false,\"jsonVersion\":\"v1.0,resolved,short\",\"platform\":\"current\",\"textVersion\":\"v2.2,js,ctx\",\"rootId\":\"ycXVAstFZ325M0PRsuXtUu7F\",\"allowExport\":false,\"id\":\"app\",\"autoColor\":\"#EEDC82\",\"deletedDecls\":[],\"name\":\"edits-test-dumb\",\"autoIcon\":\"Exit\",\"hasUniqueIds\":false,\"decls\":[{\"id\":\"SZwwuN9ffv5TLJuO8buwjifz\",\"inParameters\":[],\"body\":[{\"id\":\"x0jQd1BtQGFLL1XBIeiT9kmL\",\"locals\":[],\"expr\":\"\",\"nodeType\":\"exprStmt\"}],\"isPrivate\":false,\"isOffloaded\":false,\"name\":\"main\",\"isAsync\":false,\"isTest\":false,\"outParameters\":[],\"nodeType\":\"action\"}],\"nodeType\":\"app\",\"comment\":\"\",\"showAd\":false}", after);
-		after = applyUpdateToProgram("{\n" + 
-				"        \"decls\": [\n" + 
-				"          \"SZwwuN9ffv5TLJuO8buwjifz\",\n" + 
-				"          \"wAHj4rpF7s1v6qxVEDsWsDWL\"\n" + 
-				"        ]\n" + 
-				"      }", "app");
-		assertEquals("{\"isLibrary\":false,\"jsonVersion\":\"v1.0,resolved,short\",\"platform\":\"current\",\"textVersion\":\"v2.2,js,ctx\",\"rootId\":\"ycXVAstFZ325M0PRsuXtUu7F\",\"allowExport\":false,\"id\":\"app\",\"autoColor\":\"#EEDC82\",\"deletedDecls\":[],\"name\":\"edits-test-dumb\",\"autoIcon\":\"Exit\",\"hasUniqueIds\":false,\"decls\":[{\"id\":\"SZwwuN9ffv5TLJuO8buwjifz\",\"inParameters\":[],\"body\":[{\"id\":\"x0jQd1BtQGFLL1XBIeiT9kmL\",\"locals\":[],\"expr\":\"\",\"nodeType\":\"exprStmt\"}],\"isPrivate\":false,\"isOffloaded\":false,\"name\":\"main\",\"isAsync\":false,\"isTest\":false,\"outParameters\":[],\"nodeType\":\"action\"},{\"id\":\"wAHj4rpF7s1v6qxVEDsWsDWL\",\"name\":\"s\",\"isTransient\":true,\"isReadonly\":false,\"comment\":\"\",\"type\":\"String\",\"nodeType\":\"data\"}],\"nodeType\":\"app\",\"comment\":\"\",\"showAd\":false}",after);
+		assertEquals(
+				"{\"isLibrary\":false,\"jsonVersion\":\"v1.0,resolved,short\",\"platform\":\"current\",\"textVersion\":\"v2.2,js,ctx\",\"rootId\":\"ycXVAstFZ325M0PRsuXtUu7F\",\"allowExport\":false,\"id\":\"app\",\"autoColor\":\"#EEDC82\",\"deletedDecls\":[],\"name\":\"edits-test-dumb\",\"autoIcon\":\"Exit\",\"hasUniqueIds\":false,\"decls\":[{\"id\":\"SZwwuN9ffv5TLJuO8buwjifz\",\"inParameters\":[],\"body\":[{\"id\":\"x0jQd1BtQGFLL1XBIeiT9kmL\",\"locals\":[],\"expr\":\"\",\"nodeType\":\"exprStmt\"}],\"isPrivate\":false,\"isOffloaded\":false,\"name\":\"main\",\"isAsync\":false,\"isTest\":false,\"outParameters\":[],\"nodeType\":\"action\"}],\"nodeType\":\"app\",\"comment\":\"\",\"showAd\":false}",
+				after);
+		after = applyUpdateToProgram("{\n" + "        \"decls\": [\n"
+				+ "          \"SZwwuN9ffv5TLJuO8buwjifz\",\n"
+				+ "          \"wAHj4rpF7s1v6qxVEDsWsDWL\"\n" + "        ]\n"
+				+ "      }", "app");
+		assertEquals(
+				"{\"isLibrary\":false,\"jsonVersion\":\"v1.0,resolved,short\",\"platform\":\"current\",\"textVersion\":\"v2.2,js,ctx\",\"rootId\":\"ycXVAstFZ325M0PRsuXtUu7F\",\"allowExport\":false,\"id\":\"app\",\"autoColor\":\"#EEDC82\",\"deletedDecls\":[],\"name\":\"edits-test-dumb\",\"autoIcon\":\"Exit\",\"hasUniqueIds\":false,\"decls\":[{\"id\":\"SZwwuN9ffv5TLJuO8buwjifz\",\"inParameters\":[],\"body\":[{\"id\":\"x0jQd1BtQGFLL1XBIeiT9kmL\",\"locals\":[],\"expr\":\"\",\"nodeType\":\"exprStmt\"}],\"isPrivate\":false,\"isOffloaded\":false,\"name\":\"main\",\"isAsync\":false,\"isTest\":false,\"outParameters\":[],\"nodeType\":\"action\"},{\"id\":\"wAHj4rpF7s1v6qxVEDsWsDWL\",\"name\":\"s\",\"isTransient\":true,\"isReadonly\":false,\"comment\":\"\",\"type\":\"String\",\"nodeType\":\"data\"}],\"nodeType\":\"app\",\"comment\":\"\",\"showAd\":false}",
+				after);
 	}
 
 	@Test
@@ -158,5 +158,4 @@ public class UpdateTest {
 				+ "      }", "SZwwuN9ffv5TLJuO8buwjifz");
 		assertEquals("{\"isLibrary\":false,\"jsonVersion\":\"v1.0,resolved,short\",\"platform\":\"current\",\"textVersion\":\"v2.2,js,ctx\",\"rootId\":\"ycXVAstFZ325M0PRsuXtUu7F\",\"allowExport\":false,\"id\":\"app\",\"autoColor\":\"#EEDC82\",\"deletedDecls\":[],\"name\":\"edits-test-dumb\",\"autoIcon\":\"Exit\",\"hasUniqueIds\":false,\"decls\":[{\"inParameters\":[],\"id\":\"SZwwuN9ffv5TLJuO8buwjifz\",\"body\":[{\"id\":\"x0jQd1BtQGFLL1XBIeiT9kmL\",\"locals\":[],\"expr\":\"'\\/0022Hello_World\\/0021\\/0022 .post_to_wall\",\"nodeType\":\"exprStmt\"},{\"id\":\"w7Xl21sA24EA0Lpp43qMgHGH\",\"locals\":[],\"expr\":\":data #wAHj4rpF7s1v6qxVEDsWsDWL ,:= 'How\\/0027s_it_going\\/003f\",\"nodeType\":\"exprStmt\"}],\"isPrivate\":false,\"name\":\"main\",\"isOffloaded\":false,\"isAsync\":false,\"isTest\":false,\"outParameters\":[],\"nodeType\":\"action\"},{\"id\":\"wAHj4rpF7s1v6qxVEDsWsDWL\",\"isTransient\":true,\"name\":\"s\",\"isReadonly\":false,\"nodeType\":\"data\",\"type\":\"String\",\"comment\":\"\"}],\"comment\":\"\",\"nodeType\":\"app\",\"showAd\":false}", after);
 	}
-	
 }
