d89509696d1b05ebcedce3136deae562fbb84678
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2013-12-03 16:27:28 -0800

    The history knows about conflicts and applying to an existing program

--- TouchDevelop/test/edu/oregonstate/cs519/touchdevelop/ast/HistoryTest.java
+++ TouchDevelop/test/edu/oregonstate/cs519/touchdevelop/ast/HistoryTest.java
@@ -114,5 +114,72 @@ public class HistoryTest {
 		assertEquals(TEST_USER,changedNode.getOwner(ASTNode.EXPRESSION));
 		assertEquals(ASTNode.BASE_OWNER,changedNode.getOwner(ASTNode.ID));
 	}
+	
+	@Test
+	public void testApplyHistoryWithConflict() {
+		ASTNode program = history.apply();
+		assertEquals(0,history.getConflicts().size());
+		history = new History(program,"[\n" + 
+				"  {\n" + 
+				"    \"time\": 1384813540,\n" + 
+				"    \"seqNo\": 1,\n" + 
+				"    \"scriptId\": null,\n" + 
+				"    \"historyId\": \"2520174872591440045349a9cc8-4836-4543-8bf0-bf7fab9c78e9\",\n" + 
+				"    \"updateSize\": 591,\n" + 
+				"    \"ast\": {\n" + 
+				"      \"textVersion\": \"v2.2,js,ctx\",\n" + 
+				"      \"jsonVersion\": \"v1.0,resolved,short\",\n" + 
+				"      \"name\": \"edits-test-dumb\",\n" + 
+				"      \"comment\": \"\",\n" + 
+				"      \"autoIcon\": \"Exit\",\n" + 
+				"      \"autoColor\": \"#EEDC82\",\n" + 
+				"      \"platform\": \"current\",\n" + 
+				"      \"rootId\": \"ycXVAstFZ325M0PRsuXtUu7F\",\n" + 
+				"      \"showAd\": false,\n" + 
+				"      \"isLibrary\": false,\n" + 
+				"      \"allowExport\": false,\n" + 
+				"      \"hasUniqueIds\": false,\n" + 
+				"      \"decls\": [\n" + 
+				"        {\n" + 
+				"          \"name\": \"main\",\n" + 
+				"          \"inParameters\": [],\n" + 
+				"          \"outParameters\": [],\n" + 
+				"          \"isPrivate\": false,\n" + 
+				"          \"isOffloaded\": false,\n" + 
+				"          \"isTest\": false,\n" + 
+				"          \"isAsync\": false,\n" + 
+				"          \"nodeType\": \"action\",\n" + 
+				"          \"body\": [\n" + 
+				"            {\n" + 
+				"              \"expr\": \"\",\n" + 
+				"              \"nodeType\": \"exprStmt\",\n" + 
+				"              \"id\": \"JOF2BhU1xtg02wXfidtW0SWv\",\n" + 
+				"              \"locals\": []\n" + 
+				"            }\n" + 
+				"          ],\n" + 
+				"          \"id\": \"z2Iug4rSoCRK0hlSjOJqGYCN\"\n" + 
+				"        }\n" + 
+				"      ],\n" + 
+				"      \"deletedDecls\": [],\n" + 
+				"      \"nodeType\": \"app\",\n" + 
+				"      \"id\": \"app\"\n" + 
+				"    }\n" + 
+				"  },\n" + 
+				"  {\n" + 
+				"    \"time\": 1384814003,\n" + 
+				"    \"seqNo\": 2,\n" + 
+				"    \"scriptId\": null,\n" + 
+				"    \"historyId\": \"2520174867968868511f8fd98e4-85f9-4cc9-83c8-eae848269af9\",\n" + 
+				"    \"updateSize\": 81,\n" + 
+				"    \"updates\": {\n" + 
+				"      \"JOF2BhU1xtg02wXfidtW0SWv\": {\n" + 
+				"        \"expr\": \"'/0022Hello_Worldbla/0021/0022 .post_to_wall\"\n" + 
+				"      }\n" + 
+				"    }\n" + 
+				"  }\n" + 
+				"]","notthesameguy");
+		program = history.apply();
+		assertEquals(1,history.getConflicts().size());
+	}
 
 }
